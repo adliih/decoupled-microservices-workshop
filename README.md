c14886cad0a54a9161f93ea75532e513e4706128
Adli Ihsan Hariadi <11794203+adliih@users.noreply.github.com>
2023-06-12 17:06:09 +0700

    init

---
+++ README.md
@@ -0,0 +1,18 @@
+# Welcome to your CDK TypeScript project
+
+This is a blank project for CDK development with TypeScript.
+
+The `cdk.json` file tells the CDK Toolkit how to execute your app.
+
+## Useful commands
+
+- `npm run build` compile typescript to js
+- `npm run watch` watch for changes and compile
+- `npm run test` perform the jest unit tests
+- `cdk deploy` deploy this stack to your default AWS account/region
+- `cdk diff` compare deployed stack with current state
+- `cdk synth` emits the synthesized CloudFormation template
+
+# Workshop
+
+https://catalog.us-east-1.prod.workshops.aws/workshops/e8738cf6-6eb0-4d1d-9e98-ae240d229535/en-US
---
+++ test/decoupled-microservices-workshop.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as DecoupledMicroservicesWorkshop from '../lib/decoupled-microservices-workshop-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/decoupled-microservices-workshop-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new DecoupledMicroservicesWorkshop.DecoupledMicroservicesWorkshopStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
