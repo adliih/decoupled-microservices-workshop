7cf90a90ee0e042bce380b205900afa2a73ad326
Adli Ihsan Hariadi <11794203+adliih@users.noreply.github.com>
2023-06-13 17:33:45 +0700

    implement sqs response queue consumer

--- lambda/ride-booking/rfq-response-queue.js
+++ lambda/ride-booking/rfq-response-queue.js
@@ -1,3 +1,46 @@
+import "./typedef";
+const { DynamoDB } = require("aws-sdk");
+
+const dynamodb = new DynamoDB();
+
+/** @param {SqsEvent} event */
 exports.handler = async (event) => {
   console.log(event);
+
+  for (const record of event.Records) {
+    await handleRecord(record);
+  }
+};
+
+/** @param {SqsRecord} record */
+const handleRecord = async (record) => {
+  /** @type {RfqResponseQueueMessage} */
+  const message = JSON.parse(record.body);
+
+  const { TABLE_NAME: TableName } = process.env;
+
+  // submit to dynamodb
+  await dynamodb
+    .updateItem({
+      TableName,
+      Key: {
+        id: { S: message.id },
+      },
+      // append the new quote into quotes field
+      UpdateExpression: `SET quotes = list_append(quotes, :quote)`,
+      // constructing quote data
+      ExpressionAttributeValues: {
+        ":quote": {
+          M: {
+            providerId: {
+              S: message.providerId,
+            },
+            fare: {
+              N: String(message.fare),
+            },
+          },
+        },
+      },
+    })
+    .promise();
 };
