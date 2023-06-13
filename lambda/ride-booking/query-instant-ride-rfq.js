4108c9d393c27892985fd80e363dd1544e3a66c5
Adli Ihsan Hariadi <11794203+adliih@users.noreply.github.com>
2023-06-13 16:02:50 +0700

    query instant ride rfq lambda impl

--- lambda/ride-booking/query-instant-ride-rfq.js
+++ lambda/ride-booking/query-instant-ride-rfq.js
@@ -1,3 +1,38 @@
+const { DynamoDB } = require("aws-sdk");
+const { randomUUID } = require("crypto");
+
+const dynamodb = new DynamoDB();
+
+/**
+ * @typedef {Object} PathParam
+ * @property {string} id
+ */
+
+/**
+ * @param {Object} event
+ * @param {PathParam} event.pathParameters
+ */
 exports.handler = async (event) => {
-  console.log(event);
+  const { TABLE_NAME: TableName } = process.env;
+
+  const { id } = event.pathParameters;
+
+  const data = await dynamodb
+    .getItem({
+      TableName,
+      Key: { id: { S: id } },
+    })
+    .promise();
+
+  console.log(data);
+
+  const response = { ...data.Item };
+
+  return {
+    statusCode: 200,
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(response),
+  };
 };
